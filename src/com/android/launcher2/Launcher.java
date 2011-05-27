3b1c9a195b819205bf36bf66d6dbe5af1a2d954f
Serban Zodian <giant@kappa.ro>
2011-05-27 05:44:01 +0300

    Launcher2:Fixed the app shortcuts disappearing issue
    This issue happens consistently after placing MarketUpdater
    shortcut into a newly created folder in the homescreen
    workspace, then download & finish installation of some new
    apps from Market, go back to homescreen, if drag any
    existing app shortcuts or create a new shortcut for any
    app, they cannot stay in the homescreen.
    
    Change-Id: Ic55558465ab2893703360804ce2563c3ffe1a1ba
    Owner	Steven Xia

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -2128,30 +2128,6 @@ public final class Launcher extends Activity
     }
 
     /**
-     * If the activity is currently paused, signal that we need to re-run the loader
-     * in onResume.
-     *
-     * This needs to be called from incoming places where resources might have been loaded
-     * while we are paused.  That is becaues the Configuration might be wrong
-     * when we're not running, and if it comes back to what it was when we
-     * were paused, we are not restarted.
-     *
-     * Implementation of the method from LauncherModel.Callbacks.
-     *
-     * @return true if we are currently paused.  The caller might be able to
-     * skip some work in that case since we will come back again.
-     */
-    public boolean setLoadOnResume() {
-        if (mPaused) {
-            Log.i(TAG, "setLoadOnResume");
-            mOnResumeNeedsLoad = true;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
      * Implementation of the method from LauncherModel.Callbacks.
      */
     public int getCurrentWorkspaceScreen() {
@@ -2195,8 +2171,6 @@ public final class Launcher extends Activity
      */
     public void bindItems(ArrayList<ItemInfo> shortcuts, int start, int end) {
 
-        setLoadOnResume();
-
         final Workspace workspace = mWorkspace;
 
         for (int i=start; i<end; i++) {
@@ -2234,7 +2208,7 @@ public final class Launcher extends Activity
      * Implementation of the method from LauncherModel.Callbacks.
      */
     public void bindFolders(HashMap<Long, FolderInfo> folders) {
-        setLoadOnResume();
+
         sFolders.clear();
         sFolders.putAll(folders);
     }
@@ -2245,7 +2219,6 @@ public final class Launcher extends Activity
      * Implementation of the method from LauncherModel.Callbacks.
      */
     public void bindAppWidget(LauncherAppWidgetInfo item) {
-        setLoadOnResume();
 
         final long start = DEBUG_WIDGETS ? SystemClock.uptimeMillis() : 0;
         if (DEBUG_WIDGETS) {
@@ -2283,7 +2256,6 @@ public final class Launcher extends Activity
      * Implementation of the method from LauncherModel.Callbacks.
      */
     public void finishBindingItems() {
-        setLoadOnResume();
 
         if (mSavedState != null) {
             if (!mWorkspace.hasFocus()) {
@@ -2330,7 +2302,7 @@ public final class Launcher extends Activity
      * Implementation of the method from LauncherModel.Callbacks.
      */
     public void bindAppsAdded(ArrayList<ApplicationInfo> apps) {
-        setLoadOnResume();
+
         removeDialog(DIALOG_CREATE_SHORTCUT);
         mAllAppsGrid.addApps(apps);
     }
@@ -2341,7 +2313,7 @@ public final class Launcher extends Activity
      * Implementation of the method from LauncherModel.Callbacks.
      */
     public void bindAppsUpdated(ArrayList<ApplicationInfo> apps) {
-        setLoadOnResume();
+
         removeDialog(DIALOG_CREATE_SHORTCUT);
         mWorkspace.updateShortcuts(apps);
         mAllAppsGrid.updateApps(apps);
