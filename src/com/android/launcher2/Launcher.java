11b7b33c1beef6d393eb7da08f6678679a0d5cec
Serban Zodian <giant@kappa.ro>
2011-05-28 20:34:41 +0300

    Revert "packages/apps: Hide wallpaper when showing allapps view"
    This reverts commit 06ddd95414424fb5f1b7fdefb24013be7d85a9df.

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -1934,7 +1934,6 @@ public final class Launcher extends Activity
     }
 
     void showAllApps(boolean animated) {
-        mWorkspace.hideWallpaper(true);
         mAllAppsGrid.zoom(1.0f, animated);
 
         ((View) mAllAppsGrid).setFocusable(true);
@@ -1993,14 +1992,6 @@ public final class Launcher extends Activity
             mAllAppsGrid.zoom(0.0f, animated);
             ((View)mAllAppsGrid).setFocusable(false);
             mWorkspace.getChildAt(mWorkspace.getCurrentScreen()).requestFocus();
-            mWorkspace.startFading(true);
-            mWorkspace.hideWallpaper(false);
-
-            // TODO: fade these two too
-            /*
-            mDeleteZone.setVisibility(View.VISIBLE);
-            mHandleView.setVisibility(View.VISIBLE);
-            */
         }
     }
 
